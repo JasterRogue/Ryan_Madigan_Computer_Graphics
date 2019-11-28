e6ca1f4a3fb28d2a9cadb8764369ee948bb6d3e6
Ryan Madigan <Ryan.Madigan@students.ittralee.ie>
2019-11-28 10:53:02 +0000

    - Updated repositroy with current files Signed-off-by: Ryan Madigan <Ryan.Madigan@students.ittralee.ie>

--- outcode/Assets/TestOutCodes.cs
+++ outcode/Assets/TestOutCodes.cs
@@ -5,13 +5,27 @@ using UnityEngine;
 
 public class TestOutCodes : MonoBehaviour
 {
-    static int resWidth = 800;
-    static int resHeight = 600;
+    static int resWidth = Screen.width;
+    static int resHeight = Screen.height;
+    Vector3[] cube;
+    Vector3[] imageAfterRotation;
+    Vector3[] imageAfterTranslate;
+    Texture2D screen;
+    private float angle;
+    Color defaultColour;
+    
 
+    Renderer ourScreen;
     // Start is called before the first frame update
     void Start()
     {
-        Vector3[] cube = new Vector3[8];
+        ourScreen = FindObjectOfType<Renderer>();
+        screen = new Texture2D(resWidth, resHeight);
+        defaultColour = new Color(screen.GetPixel(1, 1).r, screen.GetPixel(1, 1).g, screen.GetPixel(1, 1).b, screen.GetPixel(1, 1).a);
+        ourScreen.material.mainTexture = screen;
+        
+
+        cube = new Vector3[8];
         cube[0] = new Vector3(1, 1, 1);
         cube[1] = new Vector3(-1, 1, 1);
         cube[2] = new Vector3(-1, -1, 1);
@@ -21,21 +35,79 @@ public class TestOutCodes : MonoBehaviour
         cube[6] = new Vector3(-1, -1, -1);
         cube[7] = new Vector3(1, -1, -1);
 
+        //Cube needed for using drawCubeV2()
+        cube[0] = new Vector3(-1, 1, 1);
+        cube[1] = new Vector3(1, 1, 1);
+        cube[2] = new Vector3(-1, -1, 1);
+        cube[3] = new Vector3(1, -1, 1);
+        cube[4] = new Vector3(-1, 1, -1);
+        cube[5] = new Vector3(1, 1, -1);
+        cube[6] = new Vector3(-1, -1, -1);
+        cube[7] = new Vector3(1, -1, -1);
+
         Vector3[] imageAfterViewingMatrix = applyViewingMatrix(cube);
 
         Vector3[] cubeAfterProjectionMatrix = applyProjectionMatrix(imageAfterViewingMatrix);
 
+      //  cubeAfterProjectionMatrix = applyTranslateMAtrix(cubeAfterProjectionMatrix);
+
+        imageAfterRotation = applyRotationMatrix(cubeAfterProjectionMatrix);
+
         cubeAfterProjectionMatrix = divide_by_z(cubeAfterProjectionMatrix);
 
-        Renderer ourScreen = FindObjectOfType<Renderer>();
-
         Vector2 v1 = new Vector2(-1f, 1f);
-        Vector2 v2 = new Vector2(1f, -1f);
+        Vector2 v2 = new Vector2(1f, -1f);    
+
+        drawCubeV2(cubeAfterProjectionMatrix, screen);
 
 
-        Texture2D screen = new Texture2D(resWidth, resHeight);
-        ourScreen.material.mainTexture = screen;
+       // floodFill(resWidth / 2, resHeight / 2, Color.yellow, defaultColour, screen);
 
+        screen.Apply();
+        
+    }
+    private Matrix4x4 translateMatrix(Vector3 v)
+    {
+        return Matrix4x4.TRS(v, Quaternion.identity, Vector3.one);
+    }
+    private Matrix4x4 rotationMatrix(Vector3 axis, float angle)
+    {  
+        Quaternion rotation = Quaternion.AngleAxis(angle, axis.normalized);
+        return
+            Matrix4x4.TRS(new Vector3(0, 0, 0),
+                            rotation,
+                            Vector3.one);
+    }
+
+    private Matrix4x4 viewingMatrix(Vector3 positionOfCamera, Vector3 target, Vector3 up)
+    {
+        return Matrix4x4.TRS(-positionOfCamera, Quaternion.LookRotation(target - positionOfCamera, up.normalized), Vector3.one);
+    }
+
+    private Vector3[] applyRotationMatrix(Vector3[] cube)
+    {
+        Vector3 startingAxis = new Vector3(15, 2, 2);
+        startingAxis.Normalize();
+        Quaternion rotation = Quaternion.AngleAxis(49, startingAxis);
+        Matrix4x4 rotationMatrix =
+            Matrix4x4.TRS(new Vector3(0, 0, 0),
+                            rotation,
+                            Vector3.one);
+        //printMatrix(rotationMatrix);
+
+        Vector3[] imageAfterRotation =
+            MatrixTransform(cube, rotationMatrix);
+
+        return imageAfterRotation;
+    }
+
+    public Vector3 calculateIfClockWiseOrAntiClockWise(Vector3 v, Vector3 u)
+    {
+        return Vector3.Cross(v, u).normalized;
+    }
+
+    public void drawCube(Vector3[] cubeAfterProjectionMatrix, Texture2D screen)
+    {
         drawLine(cubeAfterProjectionMatrix[0], cubeAfterProjectionMatrix[1], screen);
         drawLine(cubeAfterProjectionMatrix[1], cubeAfterProjectionMatrix[2], screen);
         drawLine(cubeAfterProjectionMatrix[2], cubeAfterProjectionMatrix[3], screen);
@@ -51,9 +123,106 @@ public class TestOutCodes : MonoBehaviour
         drawLine(cubeAfterProjectionMatrix[2], cubeAfterProjectionMatrix[6], screen);
         drawLine(cubeAfterProjectionMatrix[3], cubeAfterProjectionMatrix[7], screen);
 
-
-        screen.Apply();
     }
+
+    /*This method is for testing to try and get the floodfill to work completely
+     If this doesn't work I will just revert back to first drawCube() */
+    public void drawCubeV2(Vector3[] cube, Texture2D screen)
+    {
+        Vector3 frontCross = Vector3.Cross(cube[0], cube[2]);
+
+        if(frontCross.z > 0)
+        {
+            //front1
+            drawLine(cube[0], cube[3], screen);
+            drawLine(cube[3], cube[1], screen);
+            drawLine(cube[1], cube[0], screen);
+
+            //Front2
+            drawLine(cube[0], cube[2], screen);
+            drawLine(cube[2], cube[3], screen);
+            drawLine(cube[3], cube[0], screen);
+        }
+
+
+        Vector3 leftCross = Vector3.Cross(cube[4], cube[7]);
+
+        if(leftCross.z > 0)
+        {
+            //Left1
+            drawLine(cube[4], cube[2], screen);
+            drawLine(cube[2], cube[0], screen);
+            drawLine(cube[0], cube[4], screen);
+
+            //Left2
+            drawLine(cube[4], cube[6], screen);
+            drawLine(cube[6], cube[2], screen);
+            drawLine(cube[2], cube[4], screen);
+        }
+
+        Vector3 topCross = Vector3.Cross(cube[4], cube[5]);
+
+        if(topCross.z > 0)
+        {
+            //Top1
+            drawLine(cube[4], cube[0], screen);
+            drawLine(cube[0], cube[1], screen);
+            drawLine(cube[1], cube[4], screen);
+
+            //Top2
+            drawLine(cube[4], cube[1], screen);
+            drawLine(cube[1], cube[5], screen);
+            drawLine(cube[5], cube[4], screen);
+        }
+
+        Vector3 backCross = Vector3.Cross(cube[4], cube[6]);
+
+        if(backCross.z > 0)
+        {
+            //Back1
+            drawLine(cube[4], cube[5], screen);
+            drawLine(cube[5], cube[7], screen);
+            drawLine(cube[7], cube[4], screen);
+
+            //Back2
+            drawLine(cube[4], cube[7], screen);
+            drawLine(cube[7], cube[6], screen);
+            drawLine(cube[6], cube[4], screen);
+        }
+
+        Vector3 rightCross = Vector3.Cross(cube[5], cube[7]);
+
+        if(rightCross.z > 0)
+        {
+            //Right1
+            drawLine(cube[5], cube[1], screen);
+            drawLine(cube[1], cube[3], screen);
+            drawLine(cube[3], cube[5], screen);
+
+            //Right2
+            drawLine(cube[5], cube[3], screen);
+            drawLine(cube[3], cube[7], screen);
+            drawLine(cube[7], cube[5], screen);
+        }
+
+        Vector3 baseCross = Vector3.Cross(cube[6], cube[3]);
+
+        if(baseCross.z > 0)
+        {
+            //Base1
+            drawLine(cube[6], cube[3], screen);
+            drawLine(cube[3], cube[2], screen);
+            drawLine(cube[2], cube[6], screen);
+
+            //Base2
+            drawLine(cube[6], cube[7], screen);
+            drawLine(cube[7], cube[3], screen);
+            drawLine(cube[3], cube[6], screen);
+        }
+
+    }
+
+
 
     private Vector3[] applyProjectionMatrix(Vector3[] imageAfterViewingMatrix)
     {
@@ -74,6 +243,15 @@ public class TestOutCodes : MonoBehaviour
         return imageAfterViewingMatrix;
     }
 
+    public Vector3[] applyTranslateMAtrix(Vector3[] cube)
+    {
+        Matrix4x4 transformMatrix = Matrix4x4.TRS(new Vector3(4,3,3),Quaternion.identity,Vector3.one);
+
+        Vector3[] imageAfterTranslate = MatrixTransform(cube, transformMatrix);
+
+        return imageAfterTranslate;
+    }
+
     private Vector3[] divide_by_z(Vector3[] cube)
     {
         List<Vector3> output = new List<Vector3>();
@@ -88,11 +266,9 @@ public class TestOutCodes : MonoBehaviour
 
         if (lineClip(ref start, ref end))
         {
-
-
-
             plot(screen, breshenham(convertXY(start), convertXY(end)));
         }
+       
     }
 
     private void plot(Texture2D screen, List<Vector2Int> list)
@@ -268,9 +444,50 @@ public class TestOutCodes : MonoBehaviour
         return output;
     }
 
+    public void floodFill(int x, int y, Color newColour, Color oldColour, Texture2D screen)
+    {
+
+       if((x < 0) || (x >= resWidth))
+        {
+            return;
+        }
+
+      if((y < 0) || (y >= resHeight))
+        {
+            return;
+        }
+
+      if(screen.GetPixel(x,y) != oldColour)
+        {
+            return;
+        }
+
+      else 
+        {
+            screen.SetPixel(x, y, newColour);
+            floodFill(x + 1, y, newColour, oldColour, screen);
+            floodFill(x, y + 1, newColour, oldColour, screen);
+            floodFill(x - 1, y, newColour, oldColour, screen);
+            floodFill(x, y - 1, newColour, oldColour, screen);
+        }
+    }
+
     // Update is called once per frame
     void Update()
     {
-        
+        Destroy(screen);
+        screen = new Texture2D(resWidth, resHeight);
+        ourScreen.material.mainTexture = screen;
+        angle += 1;
+        Matrix4x4 persp = Matrix4x4.Perspective(45, 1.6f, 1, 1000);
+        Matrix4x4 View = viewingMatrix(new Vector3(0, 0, 10), new Vector3(0, 0, 0), new Vector3(0, 1, 0));
+        Matrix4x4 world = rotationMatrix(new Vector3(1, 1, 0), angle) * translateMatrix(new Vector3(2, 1, 3));;
+        Matrix4x4 overAll =persp * View*world;
+       
+        drawCubeV2(divide_by_z(MatrixTransform(cube, overAll)), screen);
+
+        //floodFill(resWidth/2, resHeight/2, Color.yellow, defaultColour, screen);
+
+        screen.Apply();
     }
 }
